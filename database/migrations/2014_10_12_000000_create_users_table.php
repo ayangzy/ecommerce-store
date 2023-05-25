4f0ea1fbdad17c157b580bc64a19d044bf18c61a
felix ayange <felixdecoder2020@gmail.com>
2023-05-25 14:20:30 +0100

    add migrations and models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number');
+            $table->text('address')->nullable();
+            $table->foreignId('country_id')->nullable()->constrained('countries');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
