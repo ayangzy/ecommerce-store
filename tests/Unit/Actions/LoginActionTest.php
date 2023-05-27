fb8b006c17cc7f9fb68c843ecf984cf7df4eb598
felix ayange <felixdecoder2020@gmail.com>
2023-05-27 12:00:40 +0100

    add Login action test

---
+++ tests/Unit/Actions/LoginActionTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit\Action;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Actions\LoginAction;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+
+
+class LoginActionTest extends TestCase
+{
+    /** @test */
+    public function login_success()
+    {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $loginRequest = new LoginRequest([
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $loginAction = new LoginAction();
+        $result = $loginAction->execute($loginRequest);
+
+        $this->assertArrayHasKey('status', $result);
+        $this->assertEquals('success', $result['status']);
+        $this->assertArrayHasKey('token', $result);
+        $this->assertArrayHasKey('expires_at', $result);
+        $this->assertArrayHasKey('user', $result);
+    }
+
+    /** @test */
+    public function login_failure()
+    {
+        User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $loginRequest = new LoginRequest([
+            'email' => 'test@example.com',
+            'password' => 'wrong_password',
+        ]);
+
+        $loginAction = new LoginAction();
+
+        $this->expectException(BadRequestException::class);
+        $this->expectExceptionMessage('The provided credentials are incorrect.');
+
+        $loginAction->execute($loginRequest);
+    }
+}
